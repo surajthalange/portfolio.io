91d510a2976900295def5925d6db1063efbb422a
SURAJ THALANGE <surajthalange18@gmail.com>
2023-03-13 19:40:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Android Developer", "Web Developer", "Blogger", "Designer"],
+        strings: ["Android Developer", "Web Developer", "Software Engineer", "Graphic Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Android Developer", "Web Developer", "Blogger", "Designer"],
+        strings: ["Android Developer", "Web Developer", "Software Engineer", "Graphic Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
